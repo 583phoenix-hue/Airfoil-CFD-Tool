a3a33ee8b1625e236269be72a272b5e813fb5952
Nathan Pranav <583phoenix@gmail.com>
2026-01-18 15:20:41 +0800

    Fix: Add X11 dependencies and update include paths

--- install_xfoil.sh
+++ install_xfoil.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Install XFOIL on Linux
-
+# Install XFOIL on Linux - Fixed for Railway/Render
 set -e
 
 echo "Installing XFOIL dependencies..."
 apt-get update
-apt-get install -y gfortran build-essential wget
+# Added libx11-dev which was missing
+apt-get install -y gfortran build-essential wget libx11-dev
 
 echo "Downloading XFOIL source..."
 cd /tmp
@@ -13,21 +13,24 @@ wget https://web.mit.edu/drela/Public/web/xfoil/xfoil6.99.tgz
 tar -xzf xfoil6.99.tgz
 
 echo "Compiling XFOIL..."
-cd Xfoil
-# Modify plot library settings for headless operation
-cd plotlib
+cd Xfoil/plotlib
+
+# CRITICAL FIX: Tell the compiler where X11 is actually located
+# We replace the hardcoded /usr/X11/include with the standard Linux path
+sed -i 's|/usr/X11/include|/usr/include/X11|g' Makefile
+
 make clean
 make
-cd ..
 
-cd orrs/bin
-make clean  
+cd ../orrs/bin
+make clean
 make osgen
-cd ../..
 
+cd ../..
 cd src
 make clean
-# Modify for no graphics
+
+# Remove the PLTOBJ link to avoid graphics issues in headless mode
 sed -i 's/PLTOBJ = .*/PLTOBJ = /g' Makefile
 make xfoil
 
